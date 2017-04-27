8a7eb3d944bc69a9b31baaa7f81a53fb5f71bd1c
aarond203 <15148602@studentmail.ul.ie>
2017-04-27 19:46:14 +0100

    Settings updated
    Settings menu updated

--- app/src/main/java/com/willoleary6gmail/cah/Settings.java
+++ app/src/main/java/com/willoleary6gmail/cah/Settings.java
@@ -3,10 +3,12 @@ package com.willoleary6gmail.cah;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
     Button bt1,logOut;
@@ -14,6 +16,13 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Typeface font = Typeface.createFromAsset(getAssets(), "helvetica-neue-lt-std-75-bold-5900e95806952.otf");
+        TextView settingTitle = (TextView) findViewById(R.id.settingHeader);
+        TextView signOut = (TextView) findViewById(R.id.logOutButton);
+        TextView backToMain = (TextView) findViewById(R.id.button);
+        settingTitle.setTypeface(font);
+        signOut.setTypeface(font);
+        backToMain.setTypeface(font);
         logOut = (Button)findViewById(R.id.logOutButton);
         bt1 = (Button) findViewById(R.id.button);
         bt1.setOnClickListener(new View.OnClickListener() {
