afaf7162eef9c39d0f6cdd95fac42f290868bb88
willoleary6 <15155528@studentmail.ul.ie>
2017-04-27 17:09:45 +0100

    Host game configuration

--- app/src/main/java/com/willoleary6gmail/cah/HostAGame.java
+++ app/src/main/java/com/willoleary6gmail/cah/HostAGame.java
@@ -5,13 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class HostAGame extends AppCompatActivity {
+    boolean swtch = true;
     Button bt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_agame);
+        final EditText password = (EditText) findViewById(R.id.LobbyPassword);
+        password.setVisibility(View.INVISIBLE);
+        Switch privteGame = (Switch) findViewById(R.id.privte);
         bt1 = (Button) findViewById(R.id.button);
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,5 +26,19 @@ public class HostAGame extends AppCompatActivity {
                 startActivity(toFindAGame);
             }
         });
+        privteGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(swtch){
+                    password.setVisibility(View.VISIBLE);
+                    swtch = false;
+                }else {
+                    password.setVisibility(View.INVISIBLE);
+                    swtch = true;
+                }
+
+            }
+        });
+
     }
 }
